fb12e9253228dd79076153897d12632520b6f964
Merin Klug <mklug@WKS-956.local>
2025-07-31 11:12:42 -0500

    filtering on task table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,37 @@
 class HomeController < ApplicationController
   def index
     @employees = Employee.all
-    @tasks = Task.all
+    @tasks_scope = Task.all
+
+    if params[:employee_name].present?
+      @tasks_scope = @tasks_scope.joins(:employee).where("LOWER(employees.name) LIKE ?", "%#{params[:employee_name].downcase}%")
+
+    end
+
+    if params[:status].present? && params[:status] != "all"
+      status_value = params[:status].downcase == "completed"
+      @tasks_scope = @tasks_scope.where(status: status_value)
+    end
+
+    if params[:priority_description].present?
+      @tasks_scope = @tasks_scope.joins(:priority)
+      .where("LOWER(priorities.description) LIKE ?", "%#{params[:priority_description].downcase}%")
+    end
     
-    sortable_columns = %w[description status priority_id employee]
+    sortable_columns = %w[description status priority employee]
     sort_column = params[:sort].presence_in(sortable_columns) || "employee"
     sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
 
-    if sort_column == "employee"
-      @tasks = Task.joins(:employee).order("employees.name #{sort_direction}")
-    else
-      @tasks = Task.order("#{sort_column} #{sort_direction}")
+    case sort_column
+      when "employee"
+        @tasks_scope = @tasks_scope.joins(:employee).order("employees.name #{sort_direction}")
+      when "priority"
+        @tasks_scope = @tasks_scope.joins(:priority).order("priorities.description #{sort_direction}")
+      else
+        @tasks_scope = @tasks_scope.order("#{sort_column} #{sort_direction}")
+      end
+
+      @tasks = @tasks_scope
+
     end
-    
-  end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
     def tasks_sortable_link(title, column)
         direction = (params[:sort] == column && params[:direction] == "asc") ? "desc" : "asc"
-        link_to title, request.params.merge(sort: column, direction: direction, format: nil)
+        link_to title, request.params.merge(sort: column, direction: direction, format: nil), class: "text-white"
     end
 end
