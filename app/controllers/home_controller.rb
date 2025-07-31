8c4d84dac9dddad21e5baca2bc7bb2cefb9d22a3
Merin Klug <mklug@WKS-956.local>
2025-07-31 09:02:43 -0500

    Task List sortable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,16 @@ class HomeController < ApplicationController
   def index
     @employees = Employee.all
     @tasks = Task.all
+    
+    sortable_columns = %w[description status priority_id employee]
+    sort_column = params[:sort].presence_in(sortable_columns) || "employee"
+    sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+
+    if sort_column == "employee"
+      @tasks = Task.joins(:employee).order("employees.name #{sort_direction}")
+    else
+      @tasks = Task.order("#{sort_column} #{sort_direction}")
+    end
+    
   end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,6 @@
 module TasksHelper
+    def tasks_sortable_link(title, column)
+        direction = (params[:sort] == column && params[:direction] == "asc") ? "desc" : "asc"
+        link_to title, request.params.merge(sort: column, direction: direction, format: nil)
+    end
 end
